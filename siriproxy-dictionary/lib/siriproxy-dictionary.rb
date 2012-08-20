8d51b4d70b6cc9470a2cf544b1c60a1f3bacf371
Cedric Boverie <ced@boverie.eu>
2012-08-20 02:37:15 +0200

    Dictionary is now in English

--- siriproxy-dictionary/lib/siriproxy-dictionary.rb
+++ siriproxy-dictionary/lib/siriproxy-dictionary.rb
@@ -10,15 +10,14 @@ require 'nokogiri'
 
 class SiriProxy::Plugin::Dictionary < SiriProxy::Plugin
 	def initialize(config)
-		#if you have custom configuration options, process them here!
 	end
   
-	listen_for /définitions? (.*)/i do |query|
+	listen_for /définitions? (.*)|definitions? (.*)|define (.*)/i do |query|
 
 		query = query.strip
-		query = query.sub('d\'','').sub('l\'','').sub('le ','').sub('du ','').sub('de ','').sub('pour ','')
+		query = query.gsub("la ","").gsub("les ","").gsub("le ","").gsub("l'","").gsub("d'","").gsub("des ","").gsub("de ","").gsub("du ","").gsub("une ","").gsub("un ","").gsub("dans ","").gsub("pour ","").gsub("the ","").gsub("of ","").gsub("for ","")
 		query = query.mb_chars.normalize(:kd).to_str.gsub(/\p{Mn}/, '')
-		url = "http://www.google.com/dictionary/json?callback=dict_api.callbacks.id100&q=#{URI.encode(query)}&sl=fr-FR&tl=fr-FR&restrict=pr%2Cde&client=te"
+		url = "http://www.google.com/dictionary/json?callback=dict_api.callbacks.id100&q=#{URI.encode(query)}&sl=#{user_language}&tl=#{user_language}&restrict=pr%2Cde&client=te"
 
 		jsonString = Net::HTTP.get(URI.parse(url))
 		jsonString = jsonString.gsub(",200,null)","").gsub("dict_api.callbacks.id100(","")
@@ -27,11 +26,13 @@ class SiriProxy::Plugin::Dictionary < SiriProxy::Plugin
 
 		if json["webDefinitions"] != nil
 			definition = json["webDefinitions"][0]["entries"][0]["terms"][0]["text"]
+		elsif user_language == "fr-FR"
+			definition = "Je n'ai trouvé aucune définition."
 		else
-			definition = "Je n'ai trouvé aucune définition"
+			definition = "I haven't found any definition."
 		end
+		
 		say definition
-
 		request_completed
 	end
 		
