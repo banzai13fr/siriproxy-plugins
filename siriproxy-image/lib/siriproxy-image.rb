3c93a9bfc65e655d4a446008c6102f109eded6cd
Cedric Boverie <ced@boverie.eu>
2012-08-20 14:22:44 +0200

    Images plugin is now in English too

--- siriproxy-image/lib/siriproxy-image.rb
+++ siriproxy-image/lib/siriproxy-image.rb
@@ -8,16 +8,16 @@ class SiriProxy::Plugin::Image < SiriProxy::Plugin
 	def initialize(config)
 	end
   
-	listen_for /image (.*)/i do |query|
+	listen_for /(photos?|images?|dessins?|illustrations?|affiche.moi|dessine.moi|pictures?|drawing|show me) (.*)/i do |ph,query|
 
 		query = query.strip
-		query = query.sub('d\'','').sub('l\'','').sub('le ','').sub('du ','').sub('de ','').sub('pour ','')
+		query = query.gsub("la ","").gsub("les ","").gsub("le ","").gsub("l'","").gsub("d'","").gsub("des ","").gsub("de ","").gsub("du ","").gsub("une ","").gsub("un ","").gsub("dans ","").gsub("pour ","").gsub("the ","").gsub("of ","").gsub("for ","")
 		url = "http://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=#{URI.encode(query)}"
 
 		jsonString = Net::HTTP.get(URI.parse(url))
 		json = JSON.parse(jsonString)
 
-		if json["responseData"]['results'] != nil
+		if json["responseData"]['results'] != nil and json["responseData"]['results'].size > 0
 			image = json['responseData']['results'][0]['unescapedUrl']
 			
 			object = SiriAddViews.new
@@ -29,7 +29,12 @@ class SiriProxy::Plugin::Image < SiriProxy::Plugin
 			send_object object
 
 		else
-			say "Je n'ai trouvé aucune image"
+			lang = user_language[0..1]
+			if lang == "fr"
+				say "Je n'ai trouvé aucune image."
+			else
+				say "I don't find any picture."
+			end
 		end
 
 		request_completed
